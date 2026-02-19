8568d466f30c155230a9fa2709e9f47903e1411b
Brandon Zerbe <zerbe@ntc.zcu.cz>
2026-02-19 09:05:21 +0100

    Entries is working (but is anti-pattern).

--- api/nomad/resolvers/entry.py
+++ api/nomad/resolvers/entry.py
@@ -7,16 +7,14 @@ import strawberry
 from ..types import Entry
 
 
-def make_nomad_query(json_query: Dict, page_size) -> Dict:
+def make_nomad_query(json_query: Dict) -> Dict:
     base_url: str = 'http://nomad-lab.eu/prod/v1/api/v1'
-    request_json = {"query": json_query}
-    request_json["pagination"] = {"page_size": page_size}
-    return requests.post(f'{base_url}/entries/query', json=request_json).json()
+    return requests.post(f'{base_url}/entries/query', json=json_query).json()
 
 
-def search_nomad_entries(json_query_str: str, page_size: int=10) -> List[Entry]:
+def search_nomad_entries(json_query_str: str) -> List[Entry]:
     entries = []
-    response = make_nomad_query(json.loads(json_query_str), page_size)
+    response = make_nomad_query(json.loads(json_query_str))
     for entry_dict in response["data"]:
         try:
             entry_dict["results"] = json.dumps(entry_dict["results"])
